d99abf1fb3af5a0f021d15789459bf2a42028362
hamza <hamzakhalem97@gmail.com>
2023-11-02 18:37:35 +0100

    update model shorty

--- myshorter/models.py
+++ myshorter/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class ShortyUrl(models.Model):
     url = models.CharField( max_length=250)
+    shotcode = models.CharField(unique=True, max_length=15)
+    updated_time = models.DateTimeField(auto_now=True, null= True)
+    timstamp = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.url
