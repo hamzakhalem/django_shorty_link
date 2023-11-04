1da2e71a673aafc1ff9fcecb6c1771a0b2e1c9f6
hamza <hamzakhalem97@gmail.com>
2023-11-04 17:08:39 +0100

    short code creation

--- myshorter/models.py
+++ myshorter/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 from .utils import create_shortcode
 # Create your models here.
+
+class shortyUrlManger(models.Manager):
+    def all(self, *args, **kwargs):
+        qs = super(shortyUrlManger, self).all(*args, **kwargs)
+        qs = qs.filter(active=True)
+        return qs
+    def refresh_shortcodes(self):
+        qs = ShortyUrl.objects.filter(id__gte=1)
+        new_code = 0
+        for q in qs:
+            q.shotcode = create_shortcode(q)
+            q.save()
+            new_code +=1 
+        return "new code: " + str(new_code)
 class ShortyUrl(models.Model):
     url = models.CharField( max_length=250)
     shotcode = models.CharField(unique=True, max_length=15)
     updated_time = models.DateTimeField(auto_now=True, null= True)
     timstamp = models.DateTimeField(auto_now_add=True, null=True)
-    
+    acrive  = models.BooleanField(default=True)
+    objects = shortyUrlManger()
+    some_random = shortyUrlManger()
     def __str__(self):
         return self.url
     
