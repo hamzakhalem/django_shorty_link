920f3bd51bcbce09d64af73525c01b645b638f42
hamza <hamzakhalem97@gmail.com>
2023-11-05 18:43:08 +0100

    adding manager command

--- myshorter/urls.py
+++ myshorter/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from .views import ShortyCBView, shorty_redirect
 
 urlpatterns = [
-
+    path('view-1', shorty_redirect),
+    path('view-2', ShortyCBView.as_view()),
 ]
--- myshorter/views.py
+++ myshorter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import View
 # Create your views here.
+def shorty_redirect(request, *args, **kwargs):
+    return HttpResponse('hello')
+
+class ShortyCBView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse("hello from view")
