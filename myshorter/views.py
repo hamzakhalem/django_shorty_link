9bfb7799dd24e21c7a9a610846d0488e463ea7b9
hamza <hamzakhalem97@gmail.com>
2023-11-07 18:00:21 +0100

    404 error

--- myshorter/views.py
+++ myshorter/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.views import View
 from .models import ShortyUrl
@@ -7,8 +7,9 @@ def shorty_redirect(request, slug):
     shortcodeurl = get_object_or_404(ShortyUrl, shotcode=slug )
     print(shortcodeurl)
     print('________________')
-    return HttpResponse('hello')
+    return redirect(shortcodeurl.url)
 
 class ShortyCBView(View):
     def get(self, request, slug):
         return HttpResponse("hello from view")
+
