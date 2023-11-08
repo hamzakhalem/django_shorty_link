f65a4e9110ac12f8c0f29f9d0bc7e5a74f461b36
hamza <hamzakhalem97@gmail.com>
2023-11-08 20:10:04 +0100

    forms for url

--- myshorter/forms.py
+++ myshorter/forms.py
@@ -1,4 +1,15 @@
+from typing import Any
 from django import forms
  
 class shortenForm(forms.Form):
     url = forms.CharField( max_length=200, required=False, label="URL:")
+
+    def clean(self):
+        cleaned_data = super(shortenForm, self).clean()
+        url = cleaned_data['url']
+        print(url)
+    
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        print(url)
+        return url
